97c8a8f9384ae24bd066ae66b05b5d45e303395a
fotini <f.batzoni@gmail.com>
2023-03-29 18:58:42 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,49 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PhysicalProductModel> cart = AddSampleData();
+            CustomerModel customer = GetCustomer();
+
+            foreach (PhysicalProductModel prod in cart)
+            {
+                prod.ShipItem(customer);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static CustomerModel GetCustomer()
+        {
+            return new CustomerModel
+            {
+                FirstName = "Tim",
+                LastName = "Barton",
+                City = "NY",
+                EmailAddress = "tim@testmail.com",
+                PhoneNumber = "1234567890",
+            };
+        }
+
+        private static List<PhysicalProductModel> AddSampleData()
+        {
+            List<PhysicalProductModel> output = new List<PhysicalProductModel>();
+
+            output.Add(new PhysicalProductModel { Title = "Nerf Footbal" });
+            output.Add(new PhysicalProductModel { Title = "T-Shirt" });
+            output.Add(new PhysicalProductModel { Title = "Hard Drive" });
+
+            return output;
+        }
+    }
+}
---
+++ DemoLibrary/PhysicalProductModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class PhysicalProductModel
+    {
+        public string Title { get; set;}
+
+        public bool HasOrderBeenCompleted { get;private set;}
+
+        public void ShipItem(CustomerModel customer)
+        {
+            if (HasOrderBeenCompleted == false) 
+            {
+                Console.WriteLine($"Simulating shipping {Title} to {customer.LastName} in {customer.City} ");
+                HasOrderBeenCompleted = true;
+            }
+        }
+    }
+}
